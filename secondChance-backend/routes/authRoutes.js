efa395a8517e8dd909e29adddf6b5b440561f470
Syed Zainulabideen <syed_zainulabideen@yahoo.com>
2026-03-14 01:16:29 +0300

    update flow

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -90,7 +90,60 @@ router.post("/login", async (req, res) => {
     });
 
     res.json({ authtoken, userName, userEmail });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+});
 
+const { body, validationResult } = require("express-validator");
+
+router.put("/update", async (req, res) => {
+  // Task 2: Validate the input using `validationResult` and return an appropriate message if you detect an error
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    logger.error("Validation errors in update request", errors.array());
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    // Task 3: Check if `email` is present in the header and throw an appropriate error message if it is not present
+    const email = req.headers.email;
+
+    if (!email) {
+      logger.error("Email not found in the request headers");
+      return res
+        .status(400)
+        .json({ error: "Email not found in the request headers" });
+    }
+
+    // Task 4: Connect to MongoDB
+    const db = await connectToDatabase();
+    const collection = db.collection("users");
+
+    // Task 5: Find the user credentials in database
+    const existingUser = await collection.findOne({ email });
+    existingUser.updatedAt = new Date();
+
+    // Task 6: Update the user credentials in the database
+    const updatedUser = await collection.findOneAndUpdate(
+      { email },
+      { $set: existingUser },
+      { returnDocument: "after" },
+    );
+
+    // Task 7: Create JWT authentication with `user._id` as a payload using the secret key from the .env file
+    const payload = {
+      user: {
+        id: updatedUser._id.toString(),
+      },
+    };
+
+    const authtoken = jwt.sign(payload, process.env.JWT_SECRET);
+
+    res.json({ authtoken });
   } catch (error) {
     res.status(500).json({
       message: "Internal server error",
